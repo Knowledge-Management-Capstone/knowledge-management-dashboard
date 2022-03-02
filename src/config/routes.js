5628224593efcff788f457642247d628f6399e9e
dianrahmaji <dianrahmaji@gmail.com>
2022-03-02 22:28:15 +0700

    adjusted NavigationBarItems

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,19 +1,34 @@
 import {
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
   HomeIcon,
-  InboxIcon,
-  UsersIcon
+  FolderIcon,
+  ChatAlt2Icon,
+  CogIcon,
+  PlusIcon,
+  UserIcon
 } from '@heroicons/react/outline'
 
 const routes = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false }
+  { name: 'Beranda', href: '/', icon: HomeIcon, current: true },
+  {
+    name: 'Dokumentasi Penelitian',
+    href: '/documentation',
+    icon: FolderIcon,
+    current: false
+  },
+  { name: 'Diskusi', href: '/discussion', icon: ChatAlt2Icon, current: false },
+  {
+    name: 'Pengaturan Repository',
+    href: '/settings',
+    icon: CogIcon,
+    current: false
+  },
+  {
+    name: 'Ajukan Repository',
+    href: '/proposal',
+    icon: PlusIcon,
+    current: false
+  },
+  { name: 'Profil', href: '/profile', icon: UserIcon, current: false }
 ]
 
 export default routes
