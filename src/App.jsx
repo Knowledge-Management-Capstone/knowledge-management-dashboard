8120b1eb3ea686bd9975ab5c5c501a1930feda18
dianrahmaji <dianrahmaji@gmail.com>
2022-03-03 09:30:19 +0700

    added login and register template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import pages from './config/pages'
 
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
     <Routes>
       {pages.map(({ route, navigation }) => (
         <Route {...route} key={navigation.name} />
       ))}
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
