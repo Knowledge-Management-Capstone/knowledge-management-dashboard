062d953c91a6065adc734ad8eb213433219ecab3
dianrahmaji <dianrahmaji@gmail.com>
2022-03-12 21:22:26 +0700

    added validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,21 @@ import BaseInput from '../components/generic/form/input/BaseInput'
 import BaseCheckbox from '../components/generic/form/input/BaseCheckbox'
 import BaseButton from '../components/generic/button/BaseButton'
 
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = 'Required'
+  }
+
+  return errors
+}
+
 const Login = () => {
   const formik = useFormik({
     initialValues: {
@@ -12,6 +27,7 @@ const Login = () => {
       password: '',
       remember: false
     },
+    validate,
     onSubmit: values => {
       console.table(values)
     }
@@ -42,6 +58,9 @@ const Login = () => {
                 value={formik.email}
                 onChange={formik.handleChange}
               />
+              {formik.touched.email && formik.errors.email ? (
+                <div>{formik.errors.email}</div>
+              ) : null}
               <BaseInput
                 id="password"
                 name="password"
@@ -50,7 +69,9 @@ const Login = () => {
                 value={formik.password}
                 onChange={formik.handleChange}
               />
-
+              {formik.touched.password && formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
               <div className="flex items-center justify-between">
                 <BaseCheckbox
                   id="remember"
