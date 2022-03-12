fd4a5227f8bec5c43ef6ce25c85e3e098170c1dc
dianrahmaji <dianrahmaji@gmail.com>
2022-03-12 21:34:43 +0700

    changed useFormik to <Formik>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { useFormik } from 'formik'
+import { Formik } from 'formik'
 import * as Yup from 'yup'
 
 import BaseInput from '../components/generic/form/input/BaseInput'
@@ -7,21 +7,6 @@ import BaseCheckbox from '../components/generic/form/input/BaseCheckbox'
 import BaseButton from '../components/generic/button/BaseButton'
 
 const Login = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-      remember: false
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().email('Invalid email address').required('Required'),
-      password: Yup.string().required('Required')
-    }),
-    onSubmit: values => {
-      console.table(values)
-    }
-  })
-
   return (
     <>
       <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -38,43 +23,65 @@ const Login = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 sm:rounded-lg sm:px-10">
-            <form className="space-y-6" onSubmit={formik.handleSubmit}>
-              <BaseInput
-                id="email"
-                label="Email"
-                type="email"
-                {...formik.getFieldProps('email')}
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <div>{formik.errors.email}</div>
-              ) : null}
-              <BaseInput
-                id="password"
-                label="Password"
-                type="password"
-                {...formik.getFieldProps('password')}
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <div>{formik.errors.password}</div>
-              ) : null}
-              <div className="flex items-center justify-between">
-                <BaseCheckbox
-                  id="remember"
-                  label="Remember me"
-                  {...formik.getFieldProps('remember')}
-                />
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+                remember: false
+              }}
+              validationSchema={Yup.object({
+                email: Yup.string()
+                  .email('Invalid email address')
+                  .required('Required'),
+                password: Yup.string().required('Required')
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  console.table(values)
+                  setSubmitting(false)
+                }, 400)
+              }}
+            >
+              {formik => (
+                <form className="space-y-6" onSubmit={formik.handleSubmit}>
+                  <BaseInput
+                    id="email"
+                    label="Email"
+                    type="email"
+                    {...formik.getFieldProps('email')}
+                  />
+                  {formik.touched.email && formik.errors.email ? (
+                    <div>{formik.errors.email}</div>
+                  ) : null}
+                  <BaseInput
+                    id="password"
+                    label="Password"
+                    type="password"
+                    {...formik.getFieldProps('password')}
+                  />
+                  {formik.touched.password && formik.errors.password ? (
+                    <div>{formik.errors.password}</div>
+                  ) : null}
+                  <div className="flex items-center justify-between">
+                    <BaseCheckbox
+                      id="remember"
+                      label="Remember me"
+                      {...formik.getFieldProps('remember')}
+                    />
 
-                <div className="text-sm">
-                  <Link
-                    to="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
-              </div>
-              <BaseButton type="submit">Sign in</BaseButton>
-            </form>
+                    <div className="text-sm">
+                      <Link
+                        to="#"
+                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        Forgot your password?
+                      </Link>
+                    </div>
+                  </div>
+                  <BaseButton type="submit">Sign in</BaseButton>
+                </form>
+              )}
+            </Formik>
 
             <div className="mt-6">
               <p className="mt-2 text-center text-sm text-gray-600">
