b8eb9bf43faabede0c6f08ea3901149b3b6a4548
dianrahmaji <dianrahmaji@gmail.com>
2022-03-13 17:20:06 +0700

    added top margin to base select error message

--- src/components/generic/form/input/BaseSelect.jsx
+++ src/components/generic/form/input/BaseSelect.jsx
@@ -16,7 +16,7 @@ const BaseSelect = ({ label, ...props }) => {
         {...props}
       />
       {meta.touched && meta.error ? (
-        <div className="text-xs text-red-500">{meta.error}</div>
+        <div className="mt-1 text-xs text-red-500">{meta.error}</div>
       ) : null}
     </div>
   )
