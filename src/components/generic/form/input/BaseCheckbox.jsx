8ca6fe8256033eea57b28bbb24c40306b1b19158
Dian Rahmaji <dianrahmaji@gmail.com>
2022-03-13 17:23:51 +0700

    KMND-18 Configured Formik and Yup (#21)
    * updated base input using useField
    
    * updated base input using useField
    
    * added top margin to base select error message

--- src/components/generic/form/input/BaseCheckbox.jsx
+++ src/components/generic/form/input/BaseCheckbox.jsx
@@ -1,13 +1,19 @@
-const BaseCheckbox = ({ id, label, ...props }) => {
+import { useField } from 'formik'
+
+const BaseCheckbox = ({ label, ...props }) => {
+  const [field] = useField(props)
   return (
     <div className="flex items-center">
       <input
-        id={id}
-        type="checkbox"
-        {...props}
         className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+        type="checkbox"
+        {...field}
+        {...props}
       />
-      <label htmlFor={id} className="ml-2 block text-sm text-gray-900">
+      <label
+        htmlFor={props.id || props.name}
+        className="ml-2 block text-sm text-gray-900"
+      >
         {label}
       </label>
     </div>
