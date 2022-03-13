c385b116977254c1b96f4fd845217635cb5cfdca
dianrahmaji <dianrahmaji@gmail.com>
2022-03-13 17:05:00 +0700

    updated base input using useField

--- src/components/generic/form/input/BaseSelect.jsx
+++ src/components/generic/form/input/BaseSelect.jsx
@@ -1,23 +1,23 @@
-const BaseSelect = ({ id, label, options, ...props }) => {
+import { useField } from 'formik'
+
+const BaseSelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props)
   return (
     <div>
-      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+      <label
+        htmlFor={props.id || props.name}
+        className="block text-sm font-medium text-gray-700"
+      >
         {label}
       </label>
       <select
-        id={id}
-        {...props}
         className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-      >
-        <option value="" disabled defaultValue>
-          Select account type
-        </option>
-        {options.map(option => (
-          <option value={option.value} key={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
     </div>
   )
 }
