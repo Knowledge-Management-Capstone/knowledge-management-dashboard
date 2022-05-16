1d07078edd6b6157061b670acc121b2628fed9dd
Dian Rahmaji <dianrahmaji@gmail.com>
2022-05-16 15:13:06 +0700

    added route guard (#31)

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import Header from '../components/common/Header'
 import NavigationBar from '../components/common/NavigationBar'
 
 const DashboardLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const navigate = useNavigate()
+
+  const { user } = useSelector(state => state.userLogin)
+
+  useEffect(() => {
+    navigate('/login', { replace: true })
+  }, [user, navigate])
 
   return (
     <>
