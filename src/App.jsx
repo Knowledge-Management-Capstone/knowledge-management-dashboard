b04ed74e1e6045e36042cae3b39bf8c09f8518f8
dianrahmaji <dianrahmaji@gmail.com>
2022-05-31 11:05:56 +0700

    config react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+import Login from '~/pages/Login'
+
 const App = () => {
   return (
-    <div>
-      <button className="px-4 py-2 sm:text-sm text-base font-medium rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 text-secondary bg-primary hover:bg-accent focus:ring-primary">
-        Click!
-      </button>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
