5c4d4998914fdfcf62fa99334a4dcf4d82b78203
dianrahmaji <dianrahmaji@gmail.com>
2022-06-01 10:31:28 +0700

    added DetailProfile and RepositoryHistory

---
+++ src/pages/UserProfile/components/DetailProfile.jsx
@@ -0,0 +1,9 @@
+const DetailProfile = () => {
+  return (
+    <div className="py-6 max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h1 className="text-2xl font-semibold text-gray-900">Profil Peneliti</h1>
+    </div>
+  )
+}
+
+export default DetailProfile
---
+++ src/pages/UserProfile/components/RepositoryHistory.jsx
@@ -0,0 +1,11 @@
+const RepositoryHistory = () => {
+  return (
+    <div className="py-6 max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h1 className="text-2xl font-semibold text-gray-900">
+        Repository Penelitian
+      </h1>
+    </div>
+  )
+}
+
+export default RepositoryHistory
