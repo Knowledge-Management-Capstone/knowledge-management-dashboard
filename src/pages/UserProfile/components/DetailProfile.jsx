952846d2f44a9a797f22969e8f9a42cb9c771677
dianrahmaji <dianrahmaji@gmail.com>
2022-06-01 11:20:40 +0700

    added margin

--- src/pages/UserProfile/components/DetailProfile.jsx
+++ src/pages/UserProfile/components/DetailProfile.jsx
@@ -19,7 +19,7 @@ const DetailProfile = () => {
   return (
     <div className="py-6 max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
       <h1 className="text-2xl font-semibold text-gray-900">Profil Peneliti</h1>
-      <div className="flex gap-20 mt-5">
+      <div className="ml-8 flex gap-20 mt-5">
         {profile.picture ? (
           <img
             className="h-40 w-40 rounded-full"
