c044fa6d8c90f6bcfc4e2c7ffee820ae5d024cc4
dianrahmaji <dianrahmaji@gmail.com>
2022-06-03 10:51:30 +0700

    added DiscussionHeader

--- src/pages/ResearchDiscussion/index.jsx
+++ src/pages/ResearchDiscussion/index.jsx
@@ -1,13 +1,12 @@
 import ChatInput from './components/ChatInput'
 import DashboardLayout from '~/layouts/DashboardLayout'
+import DiscussionHeader from './components/DiscussionHeader'
 
 const RepositoryDiscussion = () => {
   return (
     <DashboardLayout>
       <div className="py-6 relative">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">Diskusi</h1>
-        </div>
+        <DiscussionHeader />
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
           <ChatInput />
         </div>
