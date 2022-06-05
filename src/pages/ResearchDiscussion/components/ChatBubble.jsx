14ba7c76cbb0306473d394a9dad4f5490bc0910c
dianrahmaji <dianrahmaji@gmail.com>
2022-06-05 10:53:26 +0700

    added added ChatBubble and ChatContainer UI

--- src/pages/ResearchDiscussion/components/ChatBubble.jsx
+++ src/pages/ResearchDiscussion/components/ChatBubble.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const ChatBubble = () => {
-  return <div>ChatBubble</div>
+const ChatBubble = ({ user }) => {
+  return (
+    <div className="text-black w-fit max-w-xl flex flex-col">
+      <p className="text-sm">Dian Rahmaji</p>
+      <p className="bg-gray-200 p-4 mb-3 rounded-xl">
+        ChatBubblesadasdasdasdasd
+      </p>
+      <p className="bg-gray-200 p-4 mb-3 rounded-xl">
+        ChatBubbleasdasd alsda sd asd; asd as
+      </p>
+    </div>
+  )
 }
 
 export default ChatBubble
