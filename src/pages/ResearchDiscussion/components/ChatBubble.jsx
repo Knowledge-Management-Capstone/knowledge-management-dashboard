5c100b206fbce5e71073511acbb22a0ae8206ad4
dianrahmaji <dianrahmaji@gmail.com>
2022-06-09 10:52:51 +0700

    added loggedInId checking for ChatBubble position

--- src/pages/ResearchDiscussion/components/ChatBubble.jsx
+++ src/pages/ResearchDiscussion/components/ChatBubble.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import clsx from 'clsx'
+
+const loggedInId = 1
 
 const ChatBubble = ({ user }) => {
   return (
-    <div className="text-black w-fit max-w-xl flex flex-col">
-      <p className="text-sm">{user}</p>
-      <p className="bg-gray-200 p-4 mb-3 rounded-xl">
-        ChatBubblesadasdasdasdasd
-      </p>
+    <div
+      className={clsx('flex', {
+        'justify-end': user._id === loggedInId
+      })}
+    >
+      <div className="text-black w-fit max-w-xl">
+        {user._id !== loggedInId && <p className="text-sm">{user.fullName}</p>}
+        <p className="bg-gray-200 p-4 mb-3 rounded-xl">
+          ChatBubblesadasdasdasdasd
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/ResearchDiscussion/components/ChatContainer.jsx
+++ src/pages/ResearchDiscussion/components/ChatContainer.jsx
@@ -1,11 +1,22 @@
 import ChatBubble from './ChatBubble'
 
+const user1 = {
+  _id: 1,
+  fullName: 'Dian Rahmaji'
+}
+
+const user2 = {
+  id: 2,
+  fullName: 'Dzakiy Harissalam'
+}
+
 const ChatContainer = () => {
   return (
     <div className="w-full mx-auto px-4 sm:px-6 md:px-14 overflow-y-scroll h-7/8">
-      <div className="flex flex-col justify-end ">
+      <div className="flex flex-col">
+        <ChatBubble user={user1} />
         {[...Array(110)].map((_, i) => (
-          <ChatBubble key={i} user="Dian Rahmaji" />
+          <ChatBubble key={i} user={user2} />
         ))}
       </div>
     </div>
