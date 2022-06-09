243fed711566a9feecc5f853a6ea0ef8f33e661b
dianrahmaji <dianrahmaji@gmail.com>
2022-06-09 10:20:32 +0700

    added user props to ChatBubble

--- src/pages/ResearchDiscussion/components/ChatBubble.jsx
+++ src/pages/ResearchDiscussion/components/ChatBubble.jsx
@@ -3,12 +3,9 @@ import React from 'react'
 const ChatBubble = ({ user }) => {
   return (
     <div className="text-black w-fit max-w-xl flex flex-col">
-      <p className="text-sm">Dian Rahmaji</p>
+      <p className="text-sm">{user}</p>
       <p className="bg-gray-200 p-4 mb-3 rounded-xl">
         ChatBubblesadasdasdasdasd
-      </p>
-      <p className="bg-gray-200 p-4 mb-3 rounded-xl">
-        ChatBubbleasdasd alsda sd asd; asd as
       </p>
     </div>
   )
--- src/pages/ResearchDiscussion/components/ChatContainer.jsx
+++ src/pages/ResearchDiscussion/components/ChatContainer.jsx
@@ -5,7 +5,7 @@ const ChatContainer = () => {
     <div className="w-full mx-auto px-4 sm:px-6 md:px-14 overflow-y-scroll h-7/8">
       <div className="flex flex-col justify-end ">
         {[...Array(110)].map((_, i) => (
-          <ChatBubble key={i} />
+          <ChatBubble key={i} user="Dian Rahmaji" />
         ))}
       </div>
     </div>
