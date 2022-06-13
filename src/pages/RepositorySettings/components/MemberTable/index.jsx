0450b6247e6a7446418a464ff374959c9ec9ee35
dianrahmaji <dianrahmaji@gmail.com>
2022-06-13 09:20:49 +0700

    added AddMemberButton

--- src/pages/RepositorySettings/components/MemberTable/index.jsx
+++ src/pages/RepositorySettings/components/MemberTable/index.jsx
@@ -1,6 +1,7 @@
-import { Fragment, useState } from 'react'
-import { PencilAltIcon, TrashIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
+import { PencilAltIcon, PlusSmIcon, TrashIcon } from '@heroicons/react/outline'
 
+import BaseIconButton from '~/components/generic/button/BaseIconButton'
 import BaseTable from '~/components/generic/table/BaseTable'
 import BaseTableItem from '~/components/generic/table/BaseTableItem'
 import MemberEditModal from './MemberEditModal'
@@ -24,15 +25,16 @@ const members = [
 ]
 
 const MemberTable = () => {
-  const [openDialog, setOpenDialog] = useState(false)
+  const [openEditDialog, setOpenEditDialog] = useState(false)
+  const [openAddDialog, setOpenAddDialog] = useState(false)
   const [selectedMember, setSelectedMember] = useState(null)
 
   const handleEdit = m => {
     setSelectedMember(m)
-    setOpenDialog(true)
+    setOpenEditDialog(true)
   }
   return (
-    <Fragment>
+    <div>
       <h2 className="mt-3 text-xl font-medium">Research Member</h2>
       <BaseTable header={header}>
         {members &&
@@ -55,12 +57,17 @@ const MemberTable = () => {
             </tr>
           ))}
       </BaseTable>
+      <div className="flex mt-3 px-8 justify-end">
+        <BaseIconButton onClick={() => setOpenAddDialog(true)}>
+          <PlusSmIcon className="h-6 w-6" aria-hidden="true" />
+        </BaseIconButton>
+      </div>
       <MemberEditModal
-        open={openDialog}
-        setOpen={setOpenDialog}
+        open={openEditDialog}
+        setOpen={setOpenEditDialog}
         initialValues={selectedMember}
       />
-    </Fragment>
+    </div>
   )
 }
 
