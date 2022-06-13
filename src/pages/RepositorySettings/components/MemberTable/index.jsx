8b9ddd7e6f9fc8324b347b9d9c0ef01a3ae48a06
dianrahmaji <dianrahmaji@gmail.com>
2022-06-13 08:54:15 +0700

    added actions in member table

--- src/pages/RepositorySettings/components/MemberTable/index.jsx
+++ src/pages/RepositorySettings/components/MemberTable/index.jsx
@@ -1,8 +1,10 @@
 import { Fragment } from 'react'
+import { PencilAltIcon, TrashIcon } from '@heroicons/react/outline'
+
 import BaseTable from '~/components/generic/table/BaseTable'
 import BaseTableItem from '~/components/generic/table/BaseTableItem'
 
-const header = ['Name', 'Faculty', 'Type', 'Role']
+const header = ['Name', 'Faculty', 'Type', 'Role', 'Action']
 const members = [
   {
     _id: 1,
@@ -32,6 +34,16 @@ const MemberTable = () => {
               <BaseTableItem>{m.faculty}</BaseTableItem>
               <BaseTableItem>{m.type}</BaseTableItem>
               <BaseTableItem>{m.role}</BaseTableItem>
+              <BaseTableItem className="relative flex gap-2">
+                <PencilAltIcon
+                  className="h-6 w-6 text-gray-400 rounded-md hover:cursor-pointer hover:text-blue-700"
+                  onClick={() => handleEdit(r)}
+                />
+                <TrashIcon
+                  className="h-6 w-6 text-gray-400 rounded-md hover:cursor-pointer hover:text-red-700"
+                  onClick={() => {}}
+                />
+              </BaseTableItem>
             </tr>
           ))}
       </BaseTable>
