48cf81071245b6f93450766b1459ce800377b8a9
dianrahmaji <dianrahmaji@gmail.com>
2022-06-13 09:09:49 +0700

    added MemberEditModal

--- src/pages/RepositorySettings/components/MemberTable/index.jsx
+++ src/pages/RepositorySettings/components/MemberTable/index.jsx
@@ -1,28 +1,36 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { PencilAltIcon, TrashIcon } from '@heroicons/react/outline'
 
 import BaseTable from '~/components/generic/table/BaseTable'
 import BaseTableItem from '~/components/generic/table/BaseTableItem'
+import MemberEditModal from './MemberEditModal'
 
 const header = ['Name', 'Faculty', 'Type', 'Role', 'Action']
 const members = [
   {
     _id: 1,
-    name: 'Dian Rahmaji',
+    fullName: 'Dian Rahmaji',
     faculty: 'Engineering',
     type: 'Student',
-    role: 'Backend Engineer'
+    role: 'backend'
   },
   {
     _id: 2,
-    name: 'Dzakiy Harissalam',
+    fullName: 'Dzakiy Harissalam',
     faculty: 'Engineering',
     type: 'Student',
-    role: 'Frontend Engineer'
+    role: 'frontend'
   }
 ]
 
 const MemberTable = () => {
+  const [openDialog, setOpenDialog] = useState(false)
+  const [selectedMember, setSelectedMember] = useState(null)
+
+  const handleEdit = m => {
+    setSelectedMember(m)
+    setOpenDialog(true)
+  }
   return (
     <Fragment>
       <h2 className="mt-3 text-xl font-medium">Research Member</h2>
@@ -30,14 +38,14 @@ const MemberTable = () => {
         {members &&
           members.map(m => (
             <tr key={m._id}>
-              <BaseTableItem>{m.name}</BaseTableItem>
+              <BaseTableItem>{m.fullName}</BaseTableItem>
               <BaseTableItem>{m.faculty}</BaseTableItem>
               <BaseTableItem>{m.type}</BaseTableItem>
               <BaseTableItem>{m.role}</BaseTableItem>
               <BaseTableItem className="relative flex gap-2">
                 <PencilAltIcon
                   className="h-6 w-6 text-gray-400 rounded-md hover:cursor-pointer hover:text-blue-700"
-                  onClick={() => handleEdit(r)}
+                  onClick={() => handleEdit(m)}
                 />
                 <TrashIcon
                   className="h-6 w-6 text-gray-400 rounded-md hover:cursor-pointer hover:text-red-700"
@@ -47,6 +55,11 @@ const MemberTable = () => {
             </tr>
           ))}
       </BaseTable>
+      <MemberEditModal
+        open={openDialog}
+        setOpen={setOpenDialog}
+        initialValues={selectedMember}
+      />
     </Fragment>
   )
 }
