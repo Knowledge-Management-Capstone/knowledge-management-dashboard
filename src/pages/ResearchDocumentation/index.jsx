c526d412e011b8a9da76ad18cd84108314f5a7d4
dianrahmaji <dianrahmaji@gmail.com>
2022-06-14 09:12:20 +0700

    added Breadcrumbs

--- src/pages/ResearchDocumentation/index.jsx
+++ src/pages/ResearchDocumentation/index.jsx
@@ -1,21 +1,21 @@
+import { ChevronRightIcon } from '@heroicons/react/solid'
+
+import BaseBreadcrumbs from '~/components/generic/breadcrumbs/BaseBreadcrumbs'
 import DashboardLayout from '~/layouts/DashboardLayout'
+
+const pages = [
+  { name: 'Capstone Projects', redirect: '#', current: false },
+  { name: 'C-251', redirect: '#', current: true }
+]
 
 const ResearchDocumentation = () => {
   return (
     <DashboardLayout>
       <div className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">
-            Dokumentasi Penelitian
-          </h1>
+          <BaseBreadcrumbs pages={pages} separator={ChevronRightIcon} />
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          {/* Replace with your content */}
-          <div className="py-4">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div>
-          {/* /End replace */}
-        </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8"></div>
       </div>
     </DashboardLayout>
   )
