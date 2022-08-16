9c3bda68692eaa352f44a79a5651f80395d08ac2
dianrahmaji <dianrahmaji@gmail.com>
2022-08-16 13:40:12 +0700

    chore: remove duplicate code in TableRow

--- src/components/generic/table/BaseTable.jsx
+++ src/components/generic/table/BaseTable.jsx
@@ -1,41 +1,36 @@
 function TableRow({ colSpan, type = 'empty' }) {
-  return type === 'empty' ? (
+  return (
     <tr>
       <td
         colSpan={colSpan}
         className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6"
       >
-        <div className="flex items-center justify-center opacity-50 italic">
-          table is empty
-        </div>
-      </td>
-    </tr>
-  ) : (
-    <tr>
-      <td
-        colSpan={colSpan}
-        className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6"
-      >
-        <div className="flex items-center justify-center">
-          <svg
-            className="animate-spin ml-3 h-5 w-5 mr-3 ..."
-            viewBox="0 0 24 24"
-            fill="white"
-          >
-            <circle
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="white"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-50"
-              fill="black"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </div>
+        {type === 'empty' ? (
+          <div className="flex items-center justify-center opacity-50 italic">
+            table is empty
+          </div>
+        ) : (
+          <div className="flex items-center justify-center">
+            <svg
+              className="animate-spin ml-3 h-5 w-5 mr-3 ..."
+              viewBox="0 0 24 24"
+              fill="white"
+            >
+              <circle
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="white"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-50"
+                fill="black"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
       </td>
     </tr>
   )
