1be5ac5fe102fe8073ef9ed69f699e2439008ada
dianrahmaji <dianrahmaji@gmail.com>
2022-08-23 11:08:00 +0700

    feat: implemented fetchNotification

--- src/layouts/DashboardLayout/index.jsx
+++ src/layouts/DashboardLayout/index.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
+import { fetchNotifications } from "~/store/actions/notificationActions";
 
 import NavigationBar from "./components/NavigationBar";
 
 function DashboardLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const {
-    data: { token },
+    data: { _id: memberId, token },
   } = useSelector((state) => state.user);
+
+  const {
+    data: { roomId },
+  } = useSelector((state) => state.chat);
 
   useEffect(() => {
     if (!token) navigate("/login");
   }, [token, navigate]);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchNotifications(memberId, roomId));
+    }
+  }, [dispatch, token, memberId, roomId]);
 
   return (
     <div className="min-h-screen">
