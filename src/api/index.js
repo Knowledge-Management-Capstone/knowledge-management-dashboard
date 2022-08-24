82ad7392444e04a526a58033cc1819a47946f8b5
dianrahmaji <dianrahmaji@gmail.com>
2022-08-24 11:55:41 +0700

    chore: moved createAxios to ~/api

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,18 @@ import user from "./user";
 import team from "./team";
 import notification from "./notification";
 
+function createAxios(token) {
+  axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+  axios.defaults.headers.post["Content-Type"] = "application/json";
+
+  if (token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
+}
+
 const chatApi = chat(axios);
 const userApi = user(axios);
 const teamApi = team(axios);
 const notificationApi = notification(axios);
 
-export { chatApi, userApi, teamApi, notificationApi };
+export { createAxios, chatApi, userApi, teamApi, notificationApi };
