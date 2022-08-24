1672fb8a7d5e94cf7747c2e22e4c1eceba5628bb
dianrahmaji <dianrahmaji@gmail.com>
2022-08-24 20:47:01 +0700

    chore: replaced axios calls with api functions

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,10 @@
 export default (client) => ({
-  // FIXME: Should user and team endpoints put under
-  fetchResearchers: (data) => client.get(`/api/user/search?param=${data}`),
+  // FIXME: Should user and team endpoints put under teams?
+  searchResearchers: (data) => client.get(`/api/user/search?param=${data}`),
   fetchAllTeams: ({ id }) => client.get(`/api/user/${id}/team?all=true`),
   fetchTeams: ({ id }) => client.get(`/api/user/${id}/team`),
   fetchAcceptedTeams: ({ id }) =>
     client.get(`/api/user/${id}/team?accepted=true`),
-  userLogin: (data) => client.post("/api/user/login", data),
-  userRegister: (data) => client.post("/api/user", data),
+  login: (data) => client.post("/api/user/login", data),
+  register: (data) => client.post("/api/user", data),
 });
