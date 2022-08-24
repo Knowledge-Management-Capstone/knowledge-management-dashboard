d4b23d34a37d2c390dc4f9988b47f4419f228ee5
dianrahmaji <dianrahmaji@gmail.com>
2022-08-24 11:07:35 +0700

    chore: added api/user.js

---
+++ src/api/user.js
@@ -0,0 +1,9 @@
+export default (client) => ({
+  fetchResearchers: (data) => client.get(`/api/user/search?param=${data}`),
+  fetchAllTeams: ({ id }) => client.get(`/api/user/${id}/team?all=true`),
+  fetchTeams: ({ id }) => client.get(`/api/user/${id}/team`),
+  fetchAcceptedTeams: ({ id }) =>
+    client.get(`/api/user/${id}/team?accepted=true`),
+  userLogin: (data) => client.post("/api/user/login", data),
+  userRegister: (data) => client.post("/api/user", data),
+});
