02a42fe55179bfae8e6420191c1e8a688561f0ec
dianrahmaji <dianrahmaji@gmail.com>
2022-08-31 09:06:13 +0700

    feat: added handleUpload and handleDelete file in BaseFileUpload

--- src/components/generic/form/BaseFileUpload.jsx
+++ src/components/generic/form/BaseFileUpload.jsx
@@ -1,5 +1,93 @@
+import { useEffect, useState } from "react";
 import { useField } from "formik";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { UploadIcon } from "@heroicons/react/solid";
+
+import { storage } from "~/config/firebase";
+import useSelectedTeam from "~/hooks/useSelectedTeam";
+
+// https://alexsidorenko.com/blog/react-list-rerender/
+function FileItem({ file, onDelete }) {
+  const [percentage, setPercentage] = useState(0);
+
+  const {
+    repository: { _id: repositoryId },
+  } = useSelectedTeam();
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const storageDir = `/${repositoryId}/${file.name}`;
+      const storageRef = ref(storage, storageDir);
+
+      const upload = uploadBytesResumable(storageRef, file);
+
+      upload.on(
+        "state_changed",
+        (snapshot) => {
+          const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          );
+          setPercentage(percent);
+        },
+        // eslint-disable-next-line no-console
+        (err) => console.log(err),
+        // eslint-disable-next-line no-console
+        () =>
+          getDownloadURL(upload.snapshot.ref).then((url) => console.log(url)),
+      );
+    };
+    uploadFile();
+  }, [file, repositoryId]);
+
+  const handleDelete = async () => {
+    try {
+      const storageDir = `/${repositoryId}/${file.name}`;
+      const storageRef = ref(storage, storageDir);
+
+      await deleteObject(storageRef);
+      // TODO: Remove from array
+      console.log("success");
+      onDelete();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div
+      className="flex w-full appearance-none items-center justify-between rounded-md border p-2 text-gray-800 shadow-sm disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+      key={file.name}
+    >
+      <span className="text-gray-800">{file.name}</span>
+      <div>
+        <span>{percentage}%</span>
+        <button
+          type="button"
+          className="ml-0.5 inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-gray-300 hover:text-gray-800 focus:bg-gray-800 focus:text-white focus:outline-none"
+          onClick={handleDelete}
+        >
+          <span className="sr-only">Remove {file.name}</span>
+          <svg
+            className="h-2.5 w-2.5"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 8 8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeWidth="1.5"
+              d="M1 1l6 6m0-6L1 7"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function BaseFileUpload({ label, ...props }) {
   const [field, meta, helpers] = useField(props);
@@ -45,32 +133,12 @@ function BaseFileUpload({ label, ...props }) {
         />
       </label>
       <div className="mt-2 text-sm text-gray-500" id="list">
-        {value.map(({ name }, index) => (
-          <div
-            className="flex w-full appearance-none items-center justify-between rounded-md border p-2 text-gray-800 shadow-sm disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
-            key={name}
-          >
-            <span className="text-gray-800">{name}</span>
-            <button
-              type="button"
-              className="ml-0.5 inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-full text-gray-500 hover:bg-gray-300 hover:text-gray-800 focus:bg-gray-800 focus:text-white focus:outline-none"
-              onClick={() => handleDelete(index)}
-            >
-              <span className="sr-only">Remove {name}</span>
-              <svg
-                className="h-2.5 w-2.5"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 8 8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeWidth="1.5"
-                  d="M1 1l6 6m0-6L1 7"
-                />
-              </svg>
-            </button>
-          </div>
+        {value.map((file, index) => (
+          <FileItem
+            file={file}
+            key={file.name}
+            onDelete={() => handleDelete(index)}
+          />
         ))}
       </div>
       {touched && error ? (
--- src/pages/ResearchDocumentation/components/document/DocumentModal.jsx
+++ src/pages/ResearchDocumentation/components/document/DocumentModal.jsx
@@ -1,4 +1,8 @@
+import { ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "~/config/firebase";
+
 import { title, note, files } from "~/utils/validation";
+import useSelectedTeam from "~/hooks/useSelectedTeam";
 
 import BaseInput from "~/components/generic/form/BaseInput";
 import FormModal from "~/components/FormModal";
@@ -6,7 +10,28 @@ import TextEditorInput from "~/components/TextEditorInput";
 import BaseFileUpload from "~/components/generic/form/BaseFileUpload";
 
 function DocumentModal(props) {
-  const handleSubmit = () => {};
+  const {
+    repository: { _id: repositoryId },
+  } = useSelectedTeam();
+
+  const handleSubmit = async (values) => {
+    // TODO: Move this to BaseFileUploadComponent
+    const { files } = values;
+    const storageDir = `/files/${repositoryId}/`;
+
+    // https://www.makeuseof.com/upload-files-to-firebase-using-reactjs/#:~:text=Add%20Firebase%20to%20React&text=const%20storage%20%3D%20getStorage(app)%3B,the%20rest%20of%20your%20app.
+    try {
+      const storageRef = await ref(storage, storageDir);
+      const documents = await Promise.all([
+        files.map((file) => uploadBytesResumable(storageRef, file)),
+      ]);
+      const urls = await Promise.all([
+        documents.map((document) => document.ref.getDownloadURL()),
+      ]);
+      console.log(urls);
+      // eslint-disable-next-line no-empty
+    } catch (_) {}
+  };
   return (
     <FormModal
       validation={{ title, note, files }}
