0900442e4b41f83621181e42034eb7a756e73216
dianrahmaji <dianrahmaji@gmail.com>
2022-09-02 22:02:15 +0700

    feat: added foldercard menu

--- src/pages/ResearchDocumentation/components/folder/FolderCardList.jsx
+++ src/pages/ResearchDocumentation/components/folder/FolderCardList.jsx
@@ -1,4 +1,8 @@
-import { DotsVerticalIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/outline";
+
+import { BaseMenu, BaseMenuItem } from "~/components/generic/menu/BaseMenu";
+import FolderInfoModal from "./FolderInfoModal";
 
 const folders = [
   {
@@ -32,29 +36,43 @@ const folders = [
 ];
 
 function FolderCard({ folder }) {
+  const [openEditFolderDialog, setOpenFolderDialog] = useState(false);
+  const handleDelete = () => {};
+
   return (
-    <li key={folder.name} className="col-span-1 flex rounded-md shadow-sm">
-      <div className="flex flex-1 items-center justify-between truncate rounded-md border-2 border-gray-200 bg-white py-3">
-        <div className="flex-1 truncate px-4 py-2 text-base">
-          <a
-            href={folder.href}
-            className="font-medium text-gray-900 hover:text-gray-600"
-          >
-            {folder.name}
-          </a>
+    <>
+      <li key={folder.name} className="col-span-1 flex rounded-md shadow-sm">
+        <div className="flex flex-1 items-center justify-between rounded-md border-2 border-gray-200 bg-white py-3">
+          <div className="flex-1 truncate px-4 py-2 text-base">
+            <a
+              href={folder.href}
+              className="font-medium text-gray-900 hover:text-gray-600"
+            >
+              {folder.name}
+            </a>
+          </div>
+
+          <div className="shrink-0 pr-2">
+            <BaseMenu>
+              <BaseMenuItem
+                icon={PencilAltIcon}
+                name="Edit"
+                onClick={() => setOpenFolderDialog(true)}
+              />
+              <BaseMenuItem
+                icon={TrashIcon}
+                name="Delete"
+                onClick={handleDelete}
+              />
+            </BaseMenu>
+          </div>
         </div>
-        {/* TODO: Implement Menu */}
-        <div className="shrink-0 pr-2">
-          <button
-            type="button"
-            className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <span className="sr-only">Open options</span>
-            <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
-          </button>
-        </div>
-      </div>
-    </li>
+      </li>
+      <FolderInfoModal
+        open={openEditFolderDialog}
+        setOpen={setOpenFolderDialog}
+      />
+    </>
   );
 }
 
