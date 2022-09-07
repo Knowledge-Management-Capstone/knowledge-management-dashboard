e36c8f3c2f3b62786f24a548ea21e44361ade753
dianrahmaji <dianrahmaji@gmail.com>
2022-09-07 15:02:51 +0700

    feat: integrated fetch folder by id with redux

--- src/pages/ResearchDocumentation/index.jsx
+++ src/pages/ResearchDocumentation/index.jsx
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { ChevronRightIcon } from "@heroicons/react/solid";
 
 import useSelectedTeam from "~/hooks/useSelectedTeam";
+import { fetchFolderById } from "~/store/actions/folderActions";
 
 import ActionContainer from "./components/ActionContainer";
 import BaseBreadcrumbs from "~/components/generic/breadcrumbs/BaseBreadcrumbs";
 import DashboardLayout from "~/layouts/DashboardLayout";
 import FolderNote from "./components/folder/FolderNote";
-
 import DocumentCardList from "./components/document/DocumentCardList";
 import FolderCardList from "./components/folder/FolderCardList";
 
 function ResearchDocumentation() {
+  const dispatch = useDispatch();
+
   const {
-    repository: { title },
+    repository: { title, root: folderId },
   } = useSelectedTeam();
+  const { data: folder } = useSelector((state) => state.folder);
+
+  useEffect(() => {
+    dispatch(fetchFolderById(folderId));
+  }, [dispatch, folderId]);
 
   const pages = [{ name: title, redirect: "#", current: true }];
 
@@ -30,6 +39,7 @@ function ResearchDocumentation() {
         <div className="mt-4 flex items-center" aria-hidden="true">
           <div className="w-full border-t border-gray-300" />
         </div>
+        <pre>{JSON.stringify(folder, 0, 2)}</pre>
         <FolderCardList />
         <FolderNote />
         <DocumentCardList />
