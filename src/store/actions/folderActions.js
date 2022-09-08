2e8f58a09614a5ef1ff6c8c6e8d4fdff47f14e7e
dianrahmaji <dianrahmaji@gmail.com>
2022-09-08 22:00:05 +0700

    feat: implemented update folder usecase from FolderCardList

--- src/store/actions/folderActions.js
+++ src/store/actions/folderActions.js
@@ -5,6 +5,7 @@ import {
   FETCH_FOLDER,
   LOADING_FOLDER,
   SET_ACTIVE_FOLDER_ID,
+  // UPDATE_FOLDER,
 } from "../constants/folderConstants";
 
 export const fetchFolderById = (folderId) => async (dispatch) => {
@@ -32,6 +33,24 @@ export const createFolder = (payload) => async (dispatch) => {
     const { data } = await folderApi.createFolder(payload);
 
     dispatch({ type: CREATE_FOLDER, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ERROR_FOLDER,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateFolder = (payload) => async (dispatch) => {
+  const { folderId, ...rest } = payload;
+  try {
+    const { data } = await folderApi.updateFolder({ folderId }, rest);
+    console.log(data);
+
+    // dispatch({ type: UPDATE_FOLDER, payload: data });
   } catch (error) {
     dispatch({
       type: ERROR_FOLDER,
