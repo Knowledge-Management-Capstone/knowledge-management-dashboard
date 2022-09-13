5b916e34a67bcffa1670486e297b9ac3451c548d
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-13 10:24:04 +0700

    feat: added addDocument to documentActions

---
+++ src/store/actions/documentActions.js
@@ -0,0 +1,27 @@
+import { documentApi } from "~/api";
+import {
+  ADD_DOCUMENT,
+  ERROR_DOCUMENT,
+  LOADING_DOCUMENT,
+} from "../constants/documentConstants";
+
+// eslint-disable-next-line import/prefer-default-export
+export const addDocument = (payload) => async (dispatch) => {
+  try {
+    const { folderId, ...rest } = payload;
+
+    dispatch({ type: LOADING_DOCUMENT });
+
+    const { data } = await documentApi.addDocument({ folderId }, rest);
+
+    dispatch({ type: ADD_DOCUMENT, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ERROR_DOCUMENT,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
