349ddc1203825f17dd8ef2fb08f734889d09f8b1
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-29 14:48:34 +0700

    feat: add AttachmentEntry

--- src/pages/ResearchDiscussion/components/AttachmentList.jsx
+++ src/pages/ResearchDiscussion/components/AttachmentList.jsx
@@ -1,17 +1,38 @@
 import { useSelector } from "react-redux";
+import prettyBytes from "pretty-bytes";
+import { XIcon } from "@heroicons/react/outline";
 
 function AttachmentEntry({ attachment }) {
-  return <div>{attachment.name}</div>;
+  return (
+    <li className="relative rounded-lg border">
+      <XIcon className="absolute right-2 top-2 z-50 h-5 w-5 hover:text-red-500" />
+      <div className="group aspect-w-10 aspect-h-7 block overflow-hidden rounded-lg bg-gray-100 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+        <div type="button" className="absolute inset-0 focus:outline-none">
+          <span className="sr-only">View details for {attachment.name}</span>
+        </div>
+      </div>
+      <div className="mt-2 flex items-start justify-between p-2">
+        <div className="flex flex-col truncate">
+          <p className="pointer-events-none truncate text-sm font-medium text-gray-900">
+            {attachment.name}.{attachment.extension}
+          </p>
+          <p className="pointer-events-none block text-sm font-medium text-gray-500">
+            {prettyBytes(attachment.size)}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default function AttachmentList() {
   const attachments = useSelector((state) => state.attachments);
 
   return (
-    <div className="flex gap-2">
+    <ul className="mx-auto mt-3 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-5 xl:gap-x-8">
       {attachments.map(({ id, file }) => (
         <AttachmentEntry key={id} attachment={file} />
       ))}
-    </div>
+    </ul>
   );
 }
